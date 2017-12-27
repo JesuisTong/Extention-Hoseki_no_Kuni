c55d82b1f398d83fa8e397e6d5c180382f1c60dd
TongZ <iamtongton@gmail.com>
2017-12-27 16:29:12 +0800

    增加了tooltips点击新建页面的功能

--- coreJs/core.js
+++ coreJs/core.js
@@ -1,6 +1,13 @@
+// 随机取背景图片
 (function () {
   "use strict"
   var bgList = ['antaku', 'cinnsar', 'dia', 'empht', 'phos'];
   var bg = document.querySelector('._bg');
   bg.style.backgroundImage = `url('/icon/GemZoom/bg_${bgList[Math.floor(Math.random() * 5)]}.jpg')`;
+}())
+
+(function () {
+    chrome.browserAction.onClicked.addListener(function () {
+      chrome.tabs.create({});
+    })
 }())
