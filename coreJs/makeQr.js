7d02516c1bf43c5e30f6adfbed447d2dd18e3881
TongZ <iamtongton@gmail.com>
2018-06-06 12:04:58 +0800

    修复了 二维码无法弹出的问题

--- coreJs/makeQr.js
+++ coreJs/makeQr.js
@@ -1,13 +1,17 @@
 (function () {
   var dom = document.querySelector('.qrCode');
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (!tabs.length) {
-      return;
-    }
-    new QRCode(dom, {
-      width: 177,
-      height: 177,
-      text: tabs[0].url
-    })
+  document.addEventListener('DOMContentLoaded', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (!tabs.length) {
+        return;
+      }
+      setTimeout(() => {
+        new QRCode(dom, {
+          width: 177,
+          height: 177,
+          text: tabs[0].url
+        })
+      }, 200);
+    });
   });
 }())
